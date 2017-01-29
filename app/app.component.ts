f99833d6383d1b704316c792a90532814d8168c0
haro28 <harol.calzada@gmail.com>
2017-01-29 02:31:04 -0500

    Router basico

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,23 +3,25 @@
 import {Component} from "angular2/core";
 import {PeliculasListComponent} from "./components/peliculas-list.component";
 import {PeliculasFooterComponent} from "./components/peliculas-footer.component";
+import {ContactoComponent} from "./components/contacto.component";
+
+import {ROUTER_DIRECTIVES, RouteConfig, Router} from "angular2/router";
+
+
 @Component({
     selector: "mi-app",
     templateUrl: "app/view/peliculas.html",
-    directives: [PeliculasListComponent, PeliculasFooterComponent],
+    directives: [PeliculasListComponent,
+                        PeliculasFooterComponent,
+                        ROUTER_DIRECTIVES,
+                        ContactoComponent],
     styleUrls: ["../assets/css/styles.css"]
-    /*
-    template: `<h1>  {{titulo}}</h1>
-                        <ul>
-                            <li> Título: <strong>{{pelicula}}</strong></li>
-                            <li> Director : <strong>{{director}}</strong></li>
-                            <li> Año: <strong>{{anio}}</strong></li>
-                        </ul>
-                        `
-    */
-
 })
 
+@RouteConfig([
+    {path: "/peliculas", name: "Peliculas", component: PeliculasListComponent, useAsDefault: true},
+    {path: "/contacto", name: "Contacto", component: ContactoComponent}
+])
 export class AppComponent{
     public titulo:string = "Peliculas con Angular 2";
 
---
+++ app/components/contacto.component.ts
@@ -0,0 +1,19 @@
+
+//tsc -w *.ts comando para crear el watcher
+import {Component} from "angular2/core";
+
+
+@Component({
+    selector: "contacto",
+    templateUrl: "app/view/contacto.html",
+
+})
+
+
+export class ContactoComponent{
+    public titulo:string = "Contacto";
+
+
+    }
+
+
